acbe472944f5e9922e7eca55229cf1287991f65c
david3684 <chan3684@gmail.com>
2024-01-19 17:14:31 +0900

    WIP Minor changes in sampling logic

--- calibration.py
+++ calibration.py
@@ -13,6 +13,7 @@ def sample_calibration_set(dataset_path, calibration_size, transform=None, batch
     :param batch_size: Batch size for the DataLoader.
     :return: DataLoader for the calibration set.
     """
+    print("Sampling Calibration set from {}...".format(dataset_path))
     dataset = datasets.ImageFolder(root=dataset_path, transform=transform)
 
     indices = np.random.choice(len(dataset), size=calibration_size, replace=False)
@@ -20,15 +21,6 @@ def sample_calibration_set(dataset_path, calibration_size, transform=None, batch
 
     # DataLoader 생성
     calibration_loader = DataLoader(calibration_dataset, batch_size=batch_size, shuffle=False)
-
+    print("Done")
     return calibration_loader
 
-#transform = transforms.Compose([
-#    transforms.Resize((224, 224)),
-#    transforms.ToTensor(),
-#    # 기타 필요한 변환들
-#])
-
-#calibration_loader = sample_calibration_set(dataset_path='path/to/imagenet', 
-#                                            calibration_size=1024, 
-#                                            transform=transform)
